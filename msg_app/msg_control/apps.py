d651c708c613f6e77a3dede5bb0b51f11dac8a37
niravp98 <86225714+niravp98@users.noreply.github.com>
2021-06-22 12:59:40 -0700

    Add files via upload

---
+++ msg_app/msg_control/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MsgControlConfig(AppConfig):
+    name = 'msg_control'
---
+++ msg_app/user_hold/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserHoldConfig(AppConfig):
+    name = 'user_hold'
